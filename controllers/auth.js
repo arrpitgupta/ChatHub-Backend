1e13da426c32c8e62955e6e99a3bd0e6de3714c7
arrpitgupta <arpit17200@gmail.com>
2025-09-21 15:00:29 +0530

    initial commit

---
+++ controllers/auth.js
@@ -0,0 +1,26 @@
+import User from "../models/User.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+
+const generateToken = (id) => jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "7d" });
+
+export const register = async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.create({ username, password });
+    res.json({ token: generateToken(user._id) });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+export const login = async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  if (!user) return res.status(400).json({ message: "User not found" });
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) return res.status(400).json({ message: "Invalid credentials" });
+
+  res.json({ token: generateToken(user._id) });
+};
